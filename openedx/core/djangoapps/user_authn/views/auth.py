30f6bbe437797dba5e4282e1c8f98c6000e126b3
Ben Lu <hilltop16@gamil.com>
2024-01-30 16:32:47 -0600

    fixup! fixup! update test

--- openedx/core/djangoapps/user_authn/views/auth.py
+++ openedx/core/djangoapps/user_authn/views/auth.py
@@ -1,6 +1,4 @@
-"""
-View to provide the auth related public signing JWK set as json.
-"""
+""" Views related to auth. """
 
 
 from common.djangoapps.util.json_request import JsonResponse
@@ -8,6 +6,9 @@ from django.conf import settings
 
 
 def get_public_signing_jwks(request):
+    """
+    View to provide the auth related public signing JWK set as json.
+    """
     jwt_dict = settings.JWT_AUTH
     if not jwt_dict.get('JWT_PUBLIC_SIGNING_JWK_SET'):
         return JsonResponse({'error': 'JWK set is not found'}, status=400)
